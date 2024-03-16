4024d4b463433ffea14a7f453e6dae6f7b75495d
parfaitandre5@gmail.com <parfaitandre5@gmail.com>
2024-03-16 08:56:09 +0100

    another part5 complete

--- part5/Blog List/utils/logger.js
+++
@@ -1,9 +0,0 @@
-function info(...params) {
-    process.env.NODE_ENV !== 'test' && console.log(...params)
-}
-
-function error(...params) {
-    process.env.NODE_ENV !== 'test' && console.error(...params)
-}
-
-module.exports = { info, error }
